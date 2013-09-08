e277e090e24f94cdb43388377ecff31df190c164
Matt Schmulen <matt.schmulen@gmail.com>
2013-09-08 10:46:16 -0700

    updated for -O option on deb install

--- strongnode-bootstrap.sh
+++ strongnode-bootstrap.sh
@@ -12,31 +12,31 @@ then
     apt-get install -y python-software-properties
     apt-get install -y vim git subversion curl
     apt-get install -y memcached build-essential
+
+    #install git
+    sudo apt-get --yes --force-yes install git
 		
-		#install git
-		sudo apt-get --yes --force-yes install git
+    # Install StrongLoop Node
+    cd /opt
+	# strongloop-node_1.1.4-1_amd64.deb
+	# sudo wget http://strongloop.com/products/downloads/5334/u-0075
+	sudo wget -O strongloop-node_amd64.deb http://strongloop.com/products/downloads/5334/u-0075
+	sudo dpkg -i strongloop-node_amd64.deb
 		
-		# Install StrongLoop Node
-		cd /opt
-			# strongloop-node_1.1.4-1_amd64.deb
-		# sudo wget http://strongloop.com/products/downloads/5334/u-0075
-		sudo wget -O strongloop-node_amd64.deb http://strongloop.com/products/downloads/5334/u-0075
-		sudo dpkg -i strongloop-node_amd64.deb
+	#install some npm components
+	sudo npm install -g forever
 		
-		#install some npm components
-		sudo npm install -g forever
-		
-		# Symlink our host strongnode-app-folder to the guest /var/strongnode-app-folder folder
+	# Symlink our host strongnode-app-folder to the guest /var/strongnode-app-folder folder
     ln -s /vagrant/strongnode-app-folder /var/strongnode-app-folder
 		
     echo "You can place other node apps in gthe strongnode-app-folder/ and find them at /var/node-app-folder/"
     echo " 'slc run /var/strongnode-app-folder/myApp/app.js' to run the strong node node app in strongnode-app-folder/myApp"
 		
     # build and run your first StrongLoop App
-		# cd /var/strongnode-app-folder
-		# slnode create web myApp
-		# cd myApp
+	# cd /var/strongnode-app-folder
+	# slnode create web myApp
+	# cd myApp
     # lsnode run app.js
-		# open a browser on your host machine to http://33.33.33.10:3000	
+	# open a browser on your host machine to http://33.33.33.10:3000	
 
 fi
