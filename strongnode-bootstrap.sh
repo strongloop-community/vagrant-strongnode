c3d2e57ffce2718bc8d8ff1e97364084ac081b95
Matt Schmulen <matt.schmulen@gmail.com>
2013-09-17 03:11:47 -0700

    bootstrap update

--- strongnode-bootstrap.sh
+++ strongnode-bootstrap.sh
@@ -18,24 +18,31 @@ then
 		
     # Install StrongLoop Node
     cd /opt
-		# strongloop-node_1.1.4-1_amd64.deb 
-		sudo wget -O strongloop-node_amd64.deb http://45ec19d3127bddec1c1d-e57051fde4dbc9469167f8c2a84830dc.r36.cf1.rackcdn.com/strongloop-suite_1.0.0-1_amd64.deb
-		sudo dpkg -i strongloop-node_amd64.deb
+	# strongloop-node_1.1.4-1_amd64.deb 
+	sudo wget -O strongloop-node_amd64.deb http://45ec19d3127bddec1c1d-e57051fde4dbc9469167f8c2a84830dc.r36.cf1.rackcdn.com/strongloop-suite_1.0.0-1_amd64.deb
+	sudo dpkg -i strongloop-node_amd64.deb
 		
-		#install some npm components
-		sudo npm install -g forever
+	#install some npm components
+	sudo npm install -g forever
 		
-		# Symlink our host strongnode-app-folder to the guest /var/strongnode-app-folder folder
+	# Symlink our host strongnode-app-folder to the guest /var/strongnode-app-folder folder
     ln -s /vagrant/strongnode-app-folder /var/strongnode-app-folder
 		
     echo "You can place other node apps in gthe strongnode-app-folder/ and find them at /var/node-app-folder/"
     echo " 'slc run /var/strongnode-app-folder/myApp/app.js' to run the strong node node app in strongnode-app-folder/myApp"
 		
-    # build and run your first StrongLoop App
-		# cd /var/strongnode-app-folder
-		# slnode create web myApp
-		# cd myApp
-    # lsnode run app.js
-		# open a browser on your host machine to http://33.33.33.10:3000	
-
+		cd /var/strongnode-app-folder;
+		slc lb project loopback-node-app;
+		cd loopback-node-app;
+		slc install;
+		slc install strong-cluster-control;
+		slc install strong-agent;
+		slc install loopback@1.0.0;
+		slc lb model product;
+		slc lb model customer;
+		slc lb model store;
+		slc run app.js;
+		echo open a browser on your host machine to http://33.33.33.10:3000 ;
+		echo open a browser on your host machine to http://33.33.33.10:3000/explorer ;;
+		
 fi
